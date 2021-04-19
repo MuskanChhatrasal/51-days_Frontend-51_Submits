fac9200a580ad8e3c23fc8e1c15a5c545cec22a3
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2021-04-19 11:55:07 +0530

    Add files via upload

---
+++ Day28/index.js
@@ -0,0 +1,9 @@
+const clip = document.querySelectorAll('.clip');
+for (let i = 0; i<clip.length; i++){
+    clip[i].addEventListener('mouseenter', function(e){
+        clip[i].play()
+    })
+    clip[i].addEventListener('mouseout', function(e){
+        clip[i].pause()
+    })
+}
